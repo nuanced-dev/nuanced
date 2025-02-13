2a873f4d2a5ebadfab342c5c2b982738083de6eb
Laila Winner <lailawinner@protonmail.ch>
2025-02-13 08:06:17 -0800

    Remove CallGraph test

--- tests/nuanced/call_graph_test.py
+++ tests/nuanced/call_graph_test.py
@@ -1,9 +1,7 @@
-from deepdiff import DeepDiff
 import inspect
 import pytest
 from nuanced.lib.call_graph import CallGraph
 from tests.fixtures.fixture_class import FixtureClass
-from tests.fixtures.other_fixture_class import OtherFixtureClass
 
 def test_init_with_defaults() -> None:
     entry_points = [inspect.getfile(FixtureClass)]
@@ -11,17 +9,3 @@ def test_init_with_defaults() -> None:
     call_graph = CallGraph(entry_points)
 
     assert call_graph.call_graph.entry_points == entry_points
-
-def test_to_dict() -> None:
-    expected = {'tests.fixtures.fixture_class': ['tests.fixtures.fixture_class.FixtureClass'], 'tests.fixtures.fixture_class.FixtureClass': [], 'tests.fixtures.other_fixture_class': ['tests.fixtures.other_fixture_class.OtherFixtureClass'], 'tests.fixtures.other_fixture_class.OtherFixtureClass': [], 'tests.fixtures.other_fixture_class.OtherFixtureClass.baz': ['tests.fixtures.fixture_class.FixtureClass.bar', 'tests.fixtures.fixture_class.FixtureClass.__init__'], 'tests.fixtures.fixture_class.FixtureClass.__init__': [], 'tests.fixtures.fixture_class.FixtureClass.bar': ['tests.fixtures.fixture_class.FixtureClass.foo'], 'tests.fixtures.fixture_class.FixtureClass.foo': []}
-    entry_points = [
-       inspect.getfile(FixtureClass),
-       inspect.getfile(OtherFixtureClass),
-    ]
-    call_graph = CallGraph(entry_points)
-    call_graph.generate()
-
-    call_graph_dict = call_graph.to_dict()
-
-    diff = DeepDiff(call_graph_dict, expected)
-    assert diff == {}
