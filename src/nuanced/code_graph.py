046bd1572f2c30bd33a9217d0245a9a1a65a0da7
Laila Winner <lailawinner@protonmail.ch>
2025-02-12 20:05:42 -0800

    Write call graph JSON to .nuanced/nuanced-graph.json

--- src/nuanced/code_graph.py
+++ src/nuanced/code_graph.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import glob
+import json
 import os
 from nuanced.lib.call_graph import CallGraph
 
@@ -12,15 +13,24 @@ class CodeGraph():
     def init(cls, path: str) -> CodeGraphResult:
         errors = []
         absolute_path = os.path.abspath(path)
+        nuanced_dirpath = f'{absolute_path}/.nuanced'
+        os.mkdir(nuanced_dirpath)
+
         eligible_filepaths = glob.glob(
                 f'**/{cls.ELIGIBLE_FILE_TYPE_PATTERN}',
                 root_dir=absolute_path,
                 recursive=True
             )
         eligible_absolute_filepaths = [absolute_path + "/" + p for p in eligible_filepaths]
+
         call_graph = CallGraph(eligible_absolute_filepaths)
         call_graph.generate()
-        code_graph = cls(call_graph=call_graph.to_dict())
+        call_graph_dict = call_graph.to_dict()
+
+        nuanced_graph_file = open(f'{nuanced_dirpath}/nuanced-graph.json', "w+")
+        nuanced_graph_file.write(json.dumps(call_graph_dict))
+
+        code_graph = cls(call_graph=call_graph_dict)
         return CodeGraphResult(errors, code_graph)
 
     def __init__(self, call_graph=dict|None) -> None:
--- tests/nuanced/code_graph_test.py
+++ tests/nuanced/code_graph_test.py
@@ -4,6 +4,8 @@ from nuanced import CodeGraph
 from nuanced.lib.call_graph import CallGraph
 
 def test_init_with_valid_path(mocker) -> None:
+    mocker.patch("os.mkdir", lambda _dirname: None)
+    mock_file = mocker.mock_open()
     path = "tests/fixtures"
     spy = mocker.spy(CallGraph, "__init__")
     expected_filepaths = [
@@ -19,7 +21,22 @@ def test_init_with_valid_path(mocker) -> None:
     for p in filepaths:
         assert p in expected_filepaths
 
+def test_init_with_valid_path_persists_code_graph(mocker) -> None:
+    mocker.patch("os.mkdir", lambda _dirname: None)
+    os_spy = mocker.spy(os, "mkdir")
+    mock_file = mocker.mock_open()
+    mocker.patch("builtins.open", mock_file)
+    expected_path = os.path.abspath("tests/fixtures/.nuanced")
+
+    CodeGraph.init("tests/fixtures")
+
+    received_dir_path = os_spy.call_args.args[0]
+    assert received_dir_path == expected_path
+    mock_file.assert_called_with(f'{expected_path}/nuanced-graph.json', "w")
+
 def test_init_with_valid_path_returns_code_graph(mocker) -> None:
+    mocker.patch("os.mkdir", lambda _dirname: None)
+    mock_file = mocker.mock_open()
     path = "tests/fixtures"
     spy = mocker.spy(CallGraph, "__init__")
     expected_filepaths = [os.path.abspath("tests/fixtures/foo.py")]
