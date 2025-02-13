50c8b9bf7ae6dd3f80a27c46f613a879718bed1a
Laila Winner <lailawinner@protonmail.ch>
2025-02-12 16:40:03 -0800

    Use deepdiff to compare expected and actual values in test

--- tests/nuanced/call_graph_test.py
+++ tests/nuanced/call_graph_test.py
@@ -1,3 +1,4 @@
+from deepdiff import DeepDiff
 import inspect
 import pytest
 from nuanced.lib.call_graph import CallGraph
@@ -12,34 +13,15 @@ def test_init_with_defaults() -> None:
     assert call_graph.call_graph.entry_points == entry_points
 
 def test_to_dict() -> None:
+    expected = {'tests.fixtures.fixture_class': ['tests.fixtures.fixture_class.FixtureClass'], 'tests.fixtures.fixture_class.FixtureClass': [], 'tests.fixtures.other_fixture_class': ['tests.fixtures.other_fixture_class.OtherFixtureClass'], 'tests.fixtures.other_fixture_class.OtherFixtureClass': [], 'tests.fixtures.other_fixture_class.OtherFixtureClass.baz': ['tests.fixtures.fixture_class.FixtureClass.bar', 'tests.fixtures.fixture_class.FixtureClass.__init__'], 'tests.fixtures.fixture_class.FixtureClass.__init__': [], 'tests.fixtures.fixture_class.FixtureClass.bar': ['tests.fixtures.fixture_class.FixtureClass.foo'], 'tests.fixtures.fixture_class.FixtureClass.foo': []}
     entry_points = [
-            inspect.getfile(FixtureClass),
-            inspect.getfile(OtherFixtureClass),
-         ]
+       inspect.getfile(FixtureClass),
+       inspect.getfile(OtherFixtureClass),
+    ]
     call_graph = CallGraph(entry_points)
     call_graph.generate()
 
     call_graph_dict = call_graph.to_dict()
 
-    # TODO: flaky because `==` compares order
-    assert call_graph_dict == {
-      "tests.fixtures.fixture_class": [
-        "tests.fixtures.fixture_class.OtherFixtureClass",
-        "tests.fixtures.fixture_class.FixtureClass"
-      ],
-      "tests.fixtures.fixture_class.FixtureClass": [],
-      "tests.fixtures.fixture_class.OtherFixtureClass": [],
-      "tests.fixtures.other_fixture_class": [
-        "tests.fixtures.other_fixture_class.OtherFixtureClass"
-      ],
-      "tests.fixtures.other_fixture_class.OtherFixtureClass": [],
-      "tests.fixtures.other_fixture_class.OtherFixtureClass.baz": [
-        "tests.fixtures.fixture_class.FixtureClass.__init__",
-        "tests.fixtures.fixture_class.FixtureClass.bar"
-      ],
-      "tests.fixtures.fixture_class.FixtureClass.__init__": [],
-      "tests.fixtures.fixture_class.FixtureClass.bar": [
-        "tests.fixtures.fixture_class.FixtureClass.foo"
-      ],
-      "tests.fixtures.fixture_class.FixtureClass.foo": [],
-    }
+    diff = DeepDiff(call_graph_dict, expected)
+    assert diff == {}
