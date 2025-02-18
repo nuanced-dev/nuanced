a8162726e60b7e9e700ead49cf61cb3cc805e2a2
Laila Winner <lailawinner@protonmail.ch>
2025-02-18 07:23:59 -0800

    Implement enrich CLI command

--- src/nuanced/cli.py
+++ src/nuanced/cli.py
@@ -1,3 +1,4 @@
+import json
 import os
 import typer
 from nuanced import CodeGraph
@@ -6,7 +7,16 @@ app = typer.Typer()
 
 @app.command()
 def enrich(function_definition_path: str):
-    print(f"Not implemented")
+    nuanced_graph_path = os.path.abspath(".nuanced/nuanced-graph.json")
+    nuanced_graph_file = open(nuanced_graph_path, "r")
+    call_graph = json.load(nuanced_graph_file)
+    code_graph = CodeGraph(call_graph=call_graph)
+    result = code_graph.enrich(function_definition_path)
+
+    if not result:
+        print(f"\"{function_definition_path}\" not found")
+    else:
+        print(json.dumps(result))
 
 @app.command()
 def init(path: str):
