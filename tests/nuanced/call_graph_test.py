bbd37e88476d68448f2f9722ec0b5c022ffe7a79
Laila Winner <lailawinner@protonmail.ch>
2025-04-30 13:27:26 -0700

    Update test

--- tests/nuanced/call_graph_test.py
+++ tests/nuanced/call_graph_test.py
@@ -44,25 +44,25 @@ def test_generate_with_package_returns_call_graph_dict() -> None:
     entry_points = [inspect.getfile(FixtureClass)]
     expected = {
         "fixtures.fixture_class": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": ["fixtures.fixture_class.FixtureClass"],
             "lineno": 1,
             "end_lineno": 11,
         },
         "fixtures.fixture_class.FixtureClass.__init__": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": [],
             "lineno": 4,
             "end_lineno": 5,
         },
         "fixtures.fixture_class.FixtureClass.foo": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": [],
             "lineno": 7,
             "end_lineno": 8,
         },
         "fixtures.fixture_class.FixtureClass.bar": {
-            "filepath": os.path.abspath("fixtures/fixture_class.py"),
+            "filepath": os.path.abspath("tests/fixtures/fixture_class.py"),
             "callees": ["fixtures.fixture_class.FixtureClass.foo"],
             "lineno": 10,
             "end_lineno": 11,
